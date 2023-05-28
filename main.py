c22f8b858892308c6013811de71ee65bcc2e14d2
Vilmo18 <yvan.vilmorin@facsciences-uy1.cm>
2023-05-28 15:44:17 +0100

    api dectection

--- main.py
+++ main.py
@@ -4,10 +4,29 @@ import json
 # importation de la bibliothèque
 import requests
 import pickle
+import json
+from flask import Flask, jsonify
+from flask_pymongo import PyMongo
+from flask_cors import CORS
+import pandas as pd
+import requests
+import random as rd
+from bson.json_util import dumps
 
-model = pickle.load(open("model.pkl", "rb"))
-prediction = model.predict([0,0,0,0])
-print(prediction)
+
+app = Flask(__name__)
+CORS(app)
+mongo_db = PyMongo(app, uri='mongodb://127.0.0.1:27017/minfopra')
+db = mongo_db.db
+
+
+@app.route('/detection/update')
+def transaction():
+
+    model = pickle.load(open("model.pkl", "rb"))
+    prediction = model.predict([0, 0, 0, 0])
+    print(prediction)
+
 
 # récupération du contenu à l'aide de la méthode get()
 """r = requests.get("https://jsonplaceholder.typicode.com/posts")
