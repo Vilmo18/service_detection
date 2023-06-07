4981523c12b4b0bcfc57905e1a9e75503be8b268
Vilmo18 <yvan.vilmorin@facsciences-uy1.cm>
2023-06-07 20:31:33 +0100

    test

--- main.py
+++ main.py
@@ -57,6 +57,11 @@ def solde():
     return json.loads(dumps(val))
 
 
+@app.route('/test')
+def test():
+    return 'ok'
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=6000)
     print("lancement")
